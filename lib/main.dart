3700a98167476431617b0919c5da7e82210d3cbb
Sergey Ruppel <sergeyruppel@users.noreply.github.com>
2024-05-30 16:09:23 +0700

    Develop to Main (#1)
    * Convert buttons column to row
    
    * Scorekeeper added
    
    * Displaying the Questions (part 1)
    
    * Displaying the Questions (part 2)
    
    * Checking the User Answer
    
    * Some refactoring
    
    * Some refactoring
    
    * Refactoring
    
    * Some fix
    
    * Update project
    
    * main.dart update
    
    * quiz_logic.dart refactoring
    
    * userScore added
    
    * Alert Dialog added
    
    * 123
    
    * AlertDialog implementaion completed
    
    * Some fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,148 @@
 import 'package:flutter/material.dart';
+import 'package:quizzy_app_flutter/quiz_logic.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const Quizzy());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Quizzy extends StatelessWidget {
+  const Quizzy({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: QuizPage(),
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  QuizPageState createState() => QuizPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class QuizPageState extends State<QuizPage> {
+  final quizLogic = QuizLogic();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  var scoreKeeper = <Icon>[];
+
+  Future<void> showAlert(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('Finished!'),
+        content: Text(quizLogic.getUserScore()),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context, 'OK');
+              setState(() {
+                scoreKeeper.clear();
+                quizLogic.updateQuestion();
+              });
+            },
+            child: const Text(
+              'OK',
+              style: TextStyle(
+                color: Colors.green,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Column(
+      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      // crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          child: Center(
+            child: Text(
+              quizLogic.getQuestionText(),
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 24.0, color: Colors.white),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ),
+        SizedBox(
+          height: 24.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: scoreKeeper,
+          ),
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(Colors.green),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'True',
+                      style: TextStyle(color: Colors.white, fontSize: 18.0),
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      scoreKeeper.add(quizLogic.checkAnswer(true));
+                    });
+                    if (quizLogic.shouldShowAlert()) {
+                      showAlert(context);
+                    } else {
+                      quizLogic.updateQuestion();
+                    }
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(Colors.red),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'False',
+                      style: TextStyle(fontSize: 18.0, color: Colors.white),
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      scoreKeeper.add(quizLogic.checkAnswer(false));
+                    });
+                    if (quizLogic.shouldShowAlert()) {
+                      showAlert(context);
+                    } else {
+                      quizLogic.updateQuestion();
+                    }
+                  },
+                ),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
     );
   }
 }
---
+++ lib/quiz_logic.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:quizzy_app_flutter/question.dart';
+
+class QuizLogic {
+  var _questionNumber = 0;
+  var _userScore = 0;
+
+  final _questionList = <Question>[
+    Question('Some cats are actually allergic to humans', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was "Moon".', true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Question(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Question(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Question(
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Question('Google was originally called "Backrub".', true),
+    Question(
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
+    Question(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
+  ];
+
+  void updateQuestion() {
+    if (_questionNumber < _questionList.length - 1) {
+      _questionNumber += 1;
+    } else {
+      _questionNumber = 0;
+      _userScore = 0;
+    }
+  }
+
+  String getQuestionText() => _questionList[_questionNumber].questionText;
+
+  Icon checkAnswer(bool userAnswer) {
+    if (userAnswer == _questionList[_questionNumber].questionAnswer) {
+      _userScore += 1;
+      return const Icon(Icons.check, color: Colors.green);
+    } else {
+      return const Icon(Icons.close, color: Colors.red);
+    }
+  }
+
+  bool shouldShowAlert() =>
+      _questionNumber + 1 == _questionList.length ? true : false;
+
+  String getUserScore() => 'Your score: $_userScore/${_questionList.length}';
+}
